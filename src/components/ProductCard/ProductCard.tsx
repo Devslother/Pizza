7691a5af85aab1a67edf2aef0266ee0f372c9b8f
Sveta Co <s.costromina@gmail.com>
2025-01-15 12:09:26 +0300

    Component ProductCard

---
+++ src/components/ProductCard/ProductCard.tsx
@@ -0,0 +1,30 @@
+import { ProductCardProps } from './ProductCsrd.props';
+import styles from './ProductCard.module.css'
+import { Link } from 'react-router';
+
+export default function ProductCard (props: ProductCardProps) {
+	return (
+		<Link to={'/'} className={styles.link}>
+		<div className={styles.card}>
+			<div className={styles.head} style={{backgroundImage: `url('${props.image}')`}}>
+				<div className={styles.price}>
+					{props.price}&nbsp;
+					<span className={styles.currency}>₽</span>
+				</div>
+				<button className={styles['add-to-cart']}>
+					<img src='/cart.svg' alt='Cart'/>
+				</button>
+				<div className={styles.rating}>
+					{props.rating}&nbsp;
+					<img src="/star.svg" alt="Star" />
+				</div>
+			</div>
+		
+			<div className={styles.footer}>
+				<div className={styles.title}>{props.title}</div>
+				<div className={styles.description}>{props.description}</div>
+			</div>
+			</div>
+		</Link>
+	)
+}
--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -1,12 +1,24 @@
 import { Headling } from '../../components/Headling/Headling'
 import Search from '../../components/Search/Search'
 import styles from './Menu.module.css'
+import ProductCard from '../../components/ProductCard/ProductCard'
 
 export function Menu () {
 	return <>
 		<div className={styles.head}>
 			<Headling>Menu</Headling>
 			<Search placeholder='Введите блюдо или состав' />
+		</div>
+
+		<div>
+			<ProductCard 
+			id={1}
+			title='Tasty'
+			description='tortilla, salad, meat, sauce'
+			rating={4.5}
+			price={300}
+			image='/product.png'
+			/>
 		</div>
 	</>
 }
