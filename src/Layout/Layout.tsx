6d96123a678395c1b133dc512616ba270d7b4395
Sveta Co <s.costromina@gmail.com>
2025-01-20 15:22:49 +0300

    Регистрация

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -2,17 +2,23 @@ import { NavLink, Outlet, useNavigate } from 'react-router-dom'
 import styles from './Layout.module.css'
 import Button from '../components/Button/Button'
 import cn from 'classnames'
-import { useDispatch } from 'react-redux'
-import { AppDispatch } from '../store/store'
-import { userActions } from '../store/user.slice'
+import { useDispatch, useSelector } from 'react-redux'
+import { AppDispatch, RootState } from '../store/store'
+import { getProfile, userActions } from '../store/user.slice'
+import { useEffect } from 'react'
 
 
 export function Layout () {
 	const navigate = useNavigate()
 	const dispatch = useDispatch<AppDispatch>()
+	const profile = useSelector((s: RootState) => s.user.profile);
 
-		const logout = () => {
-			dispatch(userActions.logout());
+	useEffect(()=> {
+		dispatch(getProfile())
+	}, [dispatch]);
+
+	const logout = () => {
+		dispatch(userActions.logout());
 	  navigate('/auth/login');
 	};
 
@@ -20,8 +26,8 @@ export function Layout () {
 	 <div className={styles.sidebar}>
 		<div className={styles.user}>
 			<img className={styles.avatar} src="/avatar.png" alt="Аватар" />
-			<div className={styles.name}>Sveta</div>
-			<div className={styles.email}>Sveta@mail.com</div>
+			<div className={styles.name}>{profile?.name}</div>
+			<div className={styles.email}>{profile?.email}</div>
 		</div>
 		<div className={styles.menu}>
 		<NavLink to='/' className={({isActive}) => cn(styles['link'], {[styles.active] : isActive})}>
