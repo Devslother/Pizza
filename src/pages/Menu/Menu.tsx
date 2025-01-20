f30e14119c0dc63b7b46c50c4f0631dfecb51479
Sveta Co <s.costromina@gmail.com>
2025-01-20 19:45:39 +0300

    Поиск товаров – filter

--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -2,7 +2,7 @@ import { Headling } from '../../components/Headling/Headling'
 import Search from '../../components/Search/Search'
 import styles from './Menu.module.css'
 import { Product } from '../../interfaces/product.interface'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, ChangeEvent } from 'react'
 import { MenuList } from './MenuList'
 import axios, { AxiosError } from 'axios';
 import { PREFIX } from '../../helpers/API'
@@ -11,11 +11,18 @@ export function Menu () {
 	const[products, setProducts] = useState<Product[]>([])
 	const[isLoading, setIsLoading] = useState<boolean>(false)
 	const[error, setError] = useState<string | undefined>()
+  const[filter, setFilter] = useState<string>();
+
+  useEffect(() => {
+		getMenu(filter)
+	}, [filter])
  
-	const getMenu = async () => {
+	const getMenu = async (name?: string) => {
 		setIsLoading(true)
 		try {
-			const {data} = await axios.get<Product[]>(`${PREFIX}/products`)
+			const {data} = await axios.get<Product[]>(`${PREFIX}/products`, {
+        params: {name}
+      })
 			setProducts(data)
 			setIsLoading(false)
 		} catch(e) {
@@ -27,20 +34,21 @@ export function Menu () {
 		}
 	}
 
-	useEffect(() => {
-		getMenu()
-	}, [])
+  const updateFilter = (e: ChangeEvent<HTMLInputElement>) => {
+	  setFilter(e.target.value)
+  }
 
 	return <>
 		<div className={styles.head}>
 			<Headling>Menu</Headling>
-			<Search placeholder='Введите блюдо или состав' />
+			<Search placeholder='Введите блюдо или состав' onChange={updateFilter} />
 		</div>
 
     <div>
       {error && <>{error}</>}
-      {!isLoading && <MenuList products={products} />}
+      {!isLoading && products.length > 0 && <MenuList products={products} />}
       {isLoading && <>Is loading products...</>}
+      {!isLoading && products.length === 0 && <>Не найдено блюд по запросу</>}
     </div>
 	</>
 }
