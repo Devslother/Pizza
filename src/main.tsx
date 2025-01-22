4a3b96d9bd03b1b8b59c61f8f3402b2077427e56
Sveta Co <s.costromina@gmail.com>
2025-01-22 19:09:12 +0300

    Измененный main

--- src/main.tsx
+++ src/main.tsx
@@ -1,44 +1,44 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import { Menu } from './pages/Menu/Menu'
-import { Cart } from './pages/Cart/Cart'
-import { Error } from './pages/Error/Error'
-import { Layout } from './Layout/Layout'
-import { Product } from './pages/Product/Product'
-import { productLoader } from './pages/Product/productLoader'
-import { AuthLayout } from './Layout/AuthLayout/AuthLayout'
-import { Login } from './pages/Login/Login'
-import { Register } from './pages/Register/Register'
-import { RequireAuth } from './helpers/RequireAuth'
-import { Provider } from 'react-redux'
-import { store } from './store/store'
-import { Success } from './pages/Success/Success'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Menu } from './pages/Menu/Menu';
+import { Cart } from './pages/Cart/Cart';
+import { Error } from './pages/Error/Error';
+import { Layout } from './Layout/Layout';
+import { Product } from './pages/Product/Product';
+import { productLoader } from './pages/Product/productLoader';
+import { AuthLayout } from './Layout/AuthLayout/AuthLayout';
+import { Login } from './pages/Login/Login';
+import { Register } from './pages/Register/Register';
+import { RequireAuth } from './helpers/RequireAuth';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
+import { Success } from './pages/Success/Success';
+import { BrowserRouter } from 'react-router-dom';
 
 const router = createBrowserRouter([
   {
-    path: '/', 
+    path: '/',
     element: <RequireAuth><Layout /></RequireAuth>,
     children: [
       {
         path: '/',
         element: <Menu />,
       },
-       {
-        path: '/success',
-        element: <Success />
+      {
+        path: 'success',
+        element: <Success />,
       },
       {
-        path: '/cart',
+        path: 'cart',
         element: <Cart />,
       },
       {
-        path: '/product/:id',
+        path: 'product/:id',
         element: <Product />,
-        errorElement: <>Error</>,
         loader: productLoader,
-      }
+      },
     ],
   },
   {
@@ -47,13 +47,13 @@ const router = createBrowserRouter([
     children: [
       {
         path: 'login',
-        element: <Login />
+        element: <Login />,
       },
       {
         path: 'register',
         element: <Register />,
-      }
-    ]
+      },
+    ],
   },
   {
     path: '*',
@@ -64,7 +64,9 @@ const router = createBrowserRouter([
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <BrowserRouter basename="/repository-name"> 
+        <RouterProvider router={router} />
+      </BrowserRouter>
     </Provider>
   </StrictMode>
 );
